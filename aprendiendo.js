0ed446d6488cbdaccf5b88e7671ff8f5eed03a00
agusale03 <134938574+agusale03@users.noreply.github.com>
2023-10-05 21:26:51 -0300

    commit inicial

---
+++ aprendiendo.js
@@ -0,0 +1,44 @@
+/* Crear variables */
+ let a = 1;
+
+ let b = 3;
+
+ /* Modifico una variable*/
+
+ a = 5
+
+
+ /* Tipos de datos primitivos
+
+ string = cadena de caracteres
+ Number = integer y float
+ boolean = verdadero o falso
+
+ */
+
+ /* Console.log() es como el print en oython*/
+
+ /*
+
+ +Suma
+ -Resta
+ / Division cociente
+ * Multipliacion
+ % Division resto
+ ** Potencia
+
+ */
+
+ 
+/* Operadores logicos 
+
+== igual
+|| distinto
+> mayor
+>= mayor o igual
+< menor
+<= menor o igual
+=== igualdad estricta (valor y tipo de dato)
+!== diferencia estricta (valor y tipo de dato)
+
+*/
